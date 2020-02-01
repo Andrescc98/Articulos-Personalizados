dfd8ccf81c4af2e33e34ee72ba0ced91c1d825a2
Andrescc98 <andresjosecc@gmail.com>
2020-02-01 12:52:11 -0400

    avance 2

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -10,13 +10,23 @@ class FilterableProductTable extends React.Component{
         this.state={
             lista
         };
+        // this.handleForm=this.handleForm.bind(this);
 
     }
-
+    handleForm(event, result){
+        console.log(event, result);
+        console.log(this.state.lista)
+        console.log(lista.filter());
+        // this.setState({
+        //     lista:)
+        // })
+    }
     render(){
         return(
             <div className="container mt-4">
-                <SearchBar className="my-3"/>
+                <div>
+                    <SearchBar callback={this.handleForm.bind(this)} className="my-3"/>
+                </div>
                 <ProductTable products={this.state.lista}/>
             </div>
         );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,16 +2,17 @@ import React from "react";
 
 class SearchBar extends React.Component{
     handleChanges(e){
-        console.log(e.target.value);
+        
+        this.props.callback(e.target.name, e.target.value);
     }
     render(){
         return(
-            <form onChange={this.handleChanges} >
+            <form  >
                 <div className="form-group">                    
-                        <input type="search" name="buscar" id="buscar" placeholder="¿Que buscas?" className="form-control"/>                    
+                        <input type="search" onChange={(e)=>this.handleChanges(e)} name="buscar" id="buscar" placeholder="¿Que buscas?" className="form-control"/>                    
                 </div>
                 <div className="form-group">
-                    <input type="checkbox" name="stock" id="stock" />
+                    <input type="checkbox" onChange={(e)=>this.handleChanges(e)} name="stock" id="stock" />
                     <label htmlFor="stock">solo productos en stock</label>
                 </div>
             </form>
